3d7befd5f76b2e73da9b0c241773f8ebfcbf60b0
Melissa Murphey <melissamurphey77@gmail.com>
2020-03-01 17:12:38 -0600

    completed search TODO

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,27 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping(value= "results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
+        model.addAttribute("columns", columnChoices);
+        ArrayList<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+
+
+
+    }
+
+
+
+
+
+
 }
